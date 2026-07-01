8ee602fd951c5b80b34ab04b264ec7a36830ee8a
xct258 <101554678+xct258@users.noreply.github.com>
2026-07-01 08:32:43 +0800

    Add files via upload

--- 容器构建脚本/init-components.sh
+++ 容器构建脚本/init-components.sh
@@ -45,28 +45,12 @@ apt install -y \
     python3 \
     python3-pip
 
-# opencc,torch,faster_whisper为语音识别相关依赖
+# 视频处理相关依赖
 pip install \
     numpy \
     matplotlib \
     scipy \
-    fastapi \
-    uvicorn[standard] \
-    jinja2 \
-    pydantic \
-    python-multipart \
-    opencc \
-    torch \
-    faster_whisper \
     --break-system-packages
-
-# 安装intel核显驱动
-apt update
-apt install -y gpg wget
-wget -qO - https://repositories.intel.com/gpu/intel-graphics.key | gpg --dearmor --output /usr/share/keyrings/intel-graphics.gpg
-echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu jammy client" | tee /etc/apt/sources.list.d/intel-gpu-jammy.list
-apt update
-apt install -y intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 va-driver-all vainfo
 
 # 安装该镜像所需要的字体
 # 创建字体目录
